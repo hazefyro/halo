f0106c2fcf011057278d6d3d4d0bdabcbd8bc0b4
haze <92948735+hazetwo@users.noreply.github.com>
2026-05-29 09:26:28 +0200

    refactor extracted FetchUserInfo function

--- go-auth/internal/oauthutil/oauthutil.go
+++ go-auth/internal/oauthutil/oauthutil.go
@@ -2,6 +2,7 @@ package oauthutil
 
 import (
 	"context"
+	"encoding/json"
 
 	goauth "github.com/haze/go-auth"
 	"golang.org/x/oauth2"
@@ -19,4 +20,25 @@ func RefreshToken(config *oauth2.Config, refreshToken string) (goauth.Token, err
 		RefreshToken: token.RefreshToken,
 		ExpiresAt:    token.Expiry,
 	}, nil
+}
+
+func FetchUserInfo(ctx context.Context, config *oauth2.Config, code, url string) (map[string]any, *oauth2.Token, error) {
+	token, err := config.Exchange(ctx, code)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	client := config.Client(ctx, token)
+	res, err := client.Get(url)
+	if err != nil {
+		return nil, nil, err
+	}
+	defer res.Body.Close()
+
+	var raw map[string]any
+	if err := json.NewDecoder(res.Body).Decode(&raw); err != nil {
+		return nil, nil, err
+	}
+
+	return raw, token, nil
 }
