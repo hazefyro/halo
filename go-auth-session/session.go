7ae82071f576a42fcf1481f26c1191e927124ca0
haze <92948735+hazetwo@users.noreply.github.com>
2026-05-30 19:43:13 +0200

    add generateID function to generate cookieID

--- go-auth-session/session.go
+++ go-auth-session/session.go
@@ -1,6 +1,10 @@
 package session
 
-import "time"
+import (
+	"crypto/rand"
+	"encoding/base64"
+	"time"
+)
 
 type SessionID string
 
@@ -14,4 +18,12 @@ type Session struct {
 
 func (s *Session) isExpired(now time.Time) bool {
 	return !s.ExpiresAt.After(now)
+}
+
+func generateID() string {
+	b := make([]byte, 32)
+	if _, err := rand.Read(b); err != nil {
+		panic(err)
+	}
+	return base64.RawURLEncoding.EncodeToString(b)
 }
