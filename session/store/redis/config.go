83f659e4e8fce3d0cd48751bdef7711ff6805240
haze <92948735+hazetwo@users.noreply.github.com>
2026-06-01 09:51:32 +0200

    add config for redis session store

---
+++ session/store/redis/config.go
@@ -0,0 +1,57 @@
+package redis
+
+import (
+	"time"
+
+	"github.com/hazefyro/auth/session"
+	"github.com/redis/go-redis/v9"
+)
+
+type Config struct {
+	Config    session.Config
+	Client    redis.Cmdable
+	TTL       time.Duration
+	KeyPrefix string
+}
+
+type Option func(*Config)
+
+func WithClient(client redis.Cmdable) Option {
+	return func(c *Config) { c.Client = client }
+}
+
+func WithTTL(ttl time.Duration) Option {
+	return func(c *Config) { c.TTL = ttl }
+}
+
+func WithKeyPrefix(keyPrefix string) Option {
+	return func(c *Config) { c.KeyPrefix = keyPrefix }
+}
+
+func defaultConfig() Config {
+	return Config{
+		TTL:       24 * time.Hour,
+		KeyPrefix: "session:",
+	}
+}
+
+func applyOption(opts []Option) Config {
+	c := defaultConfig()
+
+	for _, opt := range opts {
+		opt(&c)
+	}
+
+	return c
+}
+
+func (c Config) validate() error {
+	if c.Client == nil {
+		return session.ErrNilClient
+	}
+	if c.TTL <= 0 {
+		return session.ErrInvalidTTL
+	}
+
+	return nil
+}
