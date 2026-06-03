7ec6cebea83dcfac06f6ebd3f9a724efc0f6dced
haze <92948735+hazetwo@users.noreply.github.com>
2026-06-04 00:13:57 +0200

    password: make hasher a WithHasher option

--- password/config.go
+++ password/config.go
@@ -1,12 +1,21 @@
 package password
 
+import "github.com/hazefyro/halo/password/hasher"
+
 type Config struct {
+	Hasher          hasher.Hasher
 	CollectName     bool
 	CollectUsername bool
 	CollectAvatar   bool
 }
 
 type Option func(*Config)
+
+// WithHasher sets the password hasher. When unset, the Manager uses
+// hasher.Default() (bcrypt).
+func WithHasher(h hasher.Hasher) Option {
+	return func(c *Config) { c.Hasher = h }
+}
 
 func WithName() Option {
 	return func(c *Config) { c.CollectName = true }
@@ -21,7 +30,7 @@ func WithAvatar() Option {
 }
 
 func applyOptions(opts []Option) Config {
-	c := Config{}
+	c := Config{Hasher: hasher.Default()}
 	for _, opt := range opts {
 		opt(&c)
 	}
